b30a90fb5711dff6c6dba31ba36edd5e02ee027a
Ryan Davis <cryandavis12@gmail.com>
2020-05-29 16:02:19 -0400

    spacing

--- lib/webex_api/event.rb
+++ lib/webex_api/event.rb
@@ -1,29 +1,29 @@
 module WebexApi
     class Event
-    attr_reader :xml
+        attr_reader :xml
 
-    def initialize(meeting_key,client)
-      @meeting_key= meeting_key
-      @email =  email
-      @client = client
-    end
+        def initialize(meeting_key,client)
+        @meeting_key= meeting_key
+        @email =  email
+        @client = client
+        end
 
-    def self.create_meeting(client,name,options={})
-      session_key = nil
-      session_request = WebexApi::EventRequest.new(client)
-      session_request.create_event(name,options)
-      if session_request.xml_response.at_xpath('//sessionKey')
-        session_key = session_request.xml_response.at_xpath('//sessionKey').text
-      end
+        def self.create_meeting(client,name,options={})
+        session_key = nil
+        session_request = WebexApi::EventRequest.new(client)
+        session_request.create_event(name,options)
+        if session_request.xml_response.at_xpath('//sessionKey')
+            session_key = session_request.xml_response.at_xpath('//sessionKey').text
+        end
 
-      if session_key
-        return {
-          key: session_key,
-          guestToken: session_request.xml_response.at_xpath('//guestToken')&.text,
-        }
-      else
-        return nil
-      end  
-    end
+        if session_key
+            return {
+            key: session_key,
+            guestToken: session_request.xml_response.at_xpath('//guestToken')&.text,
+            }
+        else
+            return nil
+        end  
+        end
     end
 end
