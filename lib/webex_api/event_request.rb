de9400d5a2a6c17fc606f7395c5eedc41f86362e
Ryan Davis <cryandavis12@gmail.com>
2020-05-29 16:01:48 -0400

    addition and deletions
    added functionality for additions and deletions of event centers

---
+++ lib/webex_api/event_request.rb
@@ -0,0 +1,68 @@
+module WebexApi
+    class EventRequest < WebexApi::Request
+
+        def initialize(client)
+            super(client)
+        end
+
+        def create_event(session_name, options={})
+            body = get_createevent_body(session_name, options)
+            perform_request(body)
+        end
+
+        def get_createevent_body(session_name, options)
+            body = webex_xml_request(@client.webex_email) do |xml|
+                xml.bodyContent('xsi:type' =>'java:com.webex.service.binding.event.CreateEvent'){
+                  get_meeting_body(xml, session_name, nil, options)
+                }
+            end
+            puts "Event_Request:"
+            puts body
+            body
+        end
+
+        def get_meeting_body(xml, session_name, meeting_key, options)
+            xml.accessControl{
+              xml.listing         'PUBLIC'
+              xml.sessionPassword options[:session_password]
+            }
+            xml.schedule{
+              xml.startDate         options[:start_date]
+              xml.duration          "12"
+            }
+            xml.metaData{
+                xml.sessionName session_name
+                xml.sessionType "9"
+                xml.description "This is a test"
+            }
+            xml.telephony{
+              xml.telephonySupport 'CALLIN'
+            }
+            if options[:emails]
+              xml.attendees{
+                options[:emails].each do |email|
+                    xml.attendee {
+                    xml.emailInvitations true
+                    xml.person {
+                        xml.email email
+                    }
+                }
+                end
+              }
+            end
+        end
+
+        def delete(session_key)
+            body = webex_xml_request(@client.webex_email) do |xml|
+                xml.bodyContent('xsi:type' => 'java:com.webex.service.binding.event.DelEvent'){
+                  xml.sessionKey session_key
+                }
+              end
+              perform_request(body)
+        end
+
+        def update
+
+        end
+    end
+end
